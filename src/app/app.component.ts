d7c9d9224538fb70d6d0b586c604a0fb81d064d0
Alex <prelyadeveloper@gmail.com>
2018-04-05 08:08:10 +0300

    added font-icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,13 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { RegistrationPage} from "../pages/registration/registration";
-
+import {ActionsPage} from "../pages/actions/actions";
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = RegistrationPage;
+  rootPage:any = ActionsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
